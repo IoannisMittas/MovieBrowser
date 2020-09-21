0c29122ac1b6cf4744963ec887eee274f4ddf3a6
Ioannis Mittas <jmittas@gmail.com>
2020-09-21 20:28:48 +0200

    Add more functionality to MovieListAdapter

--- app/src/main/java/com/mittas/moviebrowser/ui/screen/MovieListAdapter.kt
+++ app/src/main/java/com/mittas/moviebrowser/ui/screen/MovieListAdapter.kt
@@ -1,13 +1,16 @@
 package com.mittas.moviebrowser.ui.screen
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mittas.moviebrowser.R
 import com.mittas.moviebrowser.domain.entity.movies.Movie
 
-class MovieListAdapter(private val clickAction: (Movie) -> Unit) :
+class MovieListAdapter(private val context: Context, private val clickAction: (Movie) -> Unit) :
     RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
 
     private var movies = listOf<Movie>()
@@ -33,10 +36,27 @@ class MovieListAdapter(private val clickAction: (Movie) -> Unit) :
     inner class ViewHolder(itemView: View, private val clickAction: (Movie) -> Unit) :
         RecyclerView.ViewHolder(itemView) {
 
-        // todo find item vies
-
+        private var imageView: ImageView = itemView.findViewById(R.id.imageView)
+        private var priceTextView: TextView = itemView.findViewById(R.id.priceTextView)
+        private var availableTextView: TextView = itemView.findViewById(R.id.availableTextView)
+        private var titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        private var subtitleTextView: TextView = itemView.findViewById(R.id.subtitleTextView)
 
         fun bind(movie: Movie) {
+            // todo imageloader image load
+
+            priceTextView.text = movie.price.toString()
+
+            // todo refactor this
+//            val availableText = if (movie.isAvailable)
+//                context.getString(R.string.available_movie)
+//            else
+//                context.getString(R.string.not_available_movie)
+//            availableTextView.text = availableText
+
+            titleTextView.text = movie.title
+            subtitleTextView.text = movie.subTitle
+
             itemView.setOnClickListener { clickAction(movie) }
         }
     }
